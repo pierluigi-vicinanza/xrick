f9d83c5d2c2de429facb69f6bf72fdb651d61821
Pierluigi Vicinanza <pierluigi.vicinanza@gmail.com>
2014-09-11 21:16:05 +0100

    tidy up: - renamed endianness #define's to match what miniz uses and what GCC usually defines (according to latest docs). - made endianness #define's available even if no default endianness functions are provided, due to miniz referring to them.

--- source/xrick/system/basic_funcs.h
+++ source/xrick/system/basic_funcs.h
@@ -17,25 +17,24 @@
 
 #ifdef __WIN32__
 /* Windows is little endian only */
-#  define __LITTLE_ENDIAN 1234
-#  define __BIG_ENDIAN    4321
-#  define __BYTE_ORDER    __LITTLE_ENDIAN
+#  define __ORDER_LITTLE_ENDIAN__ 1234
+#  define __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
 #  define USE_DEFAULT_ENDIANNESS_FUNCTIONS
 #  include <stdlib.h> /* _byteswap_XXX */
 
 #elif (defined(__FreeBSD__) && __FreeBSD_version >= 470000) || defined(__OpenBSD__) || defined(__NetBSD__)
 /*  *BSD  */
 #  include <sys/endian.h>
-#  define __BIG_ENDIAN    BIG_ENDIAN
-#  define __LITTLE_ENDIAN LITTLE_ENDIAN
-#  define __BYTE_ORDER    BYTE_ORDER
+#  define __ORDER_BIG_ENDIAN__ BIG_ENDIAN
+#  define __ORDER_LITTLE_ENDIAN__ LITTLE_ENDIAN
+#  define __BYTE_ORDER__ BYTE_ORDER
 
 #elif (defined(BSD) && (BSD >= 199103)) || defined(__MacOSX__)
 /*  more BSD  */
 #  include <machine/endian.h>
-#  define __BIG_ENDIAN    BIG_ENDIAN
-#  define __LITTLE_ENDIAN LITTLE_ENDIAN
-#  define __BYTE_ORDER    BYTE_ORDER
+#  define __ORDER_BIG_ENDIAN__ BIG_ENDIAN
+#  define __ORDER_LITTLE_ENDIAN__ LITTLE_ENDIAN
+#  define __BYTE_ORDER__ BYTE_ORDER
 
 #elif defined(__linux__) /*|| defined (__BEOS__)*/
 /* Linux, BeOS */
@@ -48,22 +47,22 @@
 
 #endif /* __WIN32__ */
 
+/* define default endianness */
+#ifndef __ORDER_LITTLE_ENDIAN__
+#  define __ORDER_LITTLE_ENDIAN__ 1234
+#endif
+
+#ifndef __ORDER_BIG_ENDIAN__
+#  define __ORDER_BIG_ENDIAN__ 4321
+#endif
+
+#ifndef __BYTE_ORDER__
+#  warning "Byte order not defined on your system, assuming little endian!"
+#  define __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
+#endif
+
 /* provide default endianness functions */
 #ifdef USE_DEFAULT_ENDIANNESS_FUNCTIONS
-
-/* define default endianness */
-#  ifndef __LITTLE_ENDIAN
-#    define __LITTLE_ENDIAN 1234
-#  endif
-
-#  ifndef __BIG_ENDIAN
-#    define __BIG_ENDIAN 4321
-#  endif
-
-#  ifndef __BYTE_ORDER
-#    warning "Byte order not defined on your system, assuming little endian!"
-#    define __BYTE_ORDER __LITTLE_ENDIAN
-#  endif
 
 #  define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 
@@ -92,7 +91,7 @@ inline uint16_t swap16(uint16_t x)
 #  endif /* _MSC_VER */
 }
 
-#  if __BYTE_ORDER == __LITTLE_ENDIAN
+#  if (__BYTE_ORDER__==__ORDER_LITTLE_ENDIAN__)
 inline uint16_t htobe16(uint16_t host) { return swap16(host);  }
 inline uint16_t htole16(uint16_t host) { return host; }
 inline uint16_t betoh16(uint16_t big_endian) { return swap16(big_endian); }
@@ -103,7 +102,7 @@ inline uint32_t htole32(uint32_t host) { return host; }
 inline uint32_t betoh32(uint32_t big_endian) { return swap32(big_endian); }
 inline uint32_t letoh32(uint32_t little_endian) { return little_endian; }
 
-#  elif __BYTE_ORDER == __BIG_ENDIAN
+#  elif (__BYTE_ORDER__==__ORDER_BIG_ENDIAN__)
 inline uint16_t htobe16(uint16_t host) { return host;  }
 inline uint16_t htole16(uint16_t host) { return swap16(host); }
 inline uint16_t betoh16(uint16_t big_endian) { return big_endian; }
